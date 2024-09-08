3ade6ddeb5a5fbe8d5fe4dbbfd1a5bc1e3a8d735
Palash A <palash.agarwal.96@gmail.com>
2024-09-08 07:38:18 -0400

    wallet pregeneration

--- client/src/context/Web3AuthContext.tsx
+++ client/src/context/Web3AuthContext.tsx
@@ -4,8 +4,9 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { Web3Auth, decodeToken } from '@web3auth/single-factor-auth';
 import { ADAPTER_EVENTS, CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from '@web3auth/base';
 import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider';
-import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import Web3 from 'web3';
+
+import { v4 as uuidv4 } from 'uuid';
 
 export const Web3AuthContext = createContext<any>(null);
 
@@ -35,12 +36,32 @@ const web3auth = new Web3Auth({
 
 export default function Web3AuthProvider({ children }: any) {
 
+  const [ uuid, setUuid ] = useState('');
+  const [ address, setAddress ] = useState('');
   const [ loggedIn, setLoggedIn ] = useState(false);
-  const [ provider, setProvider ] = useState<IProvider | null>(null);  
+  const [ provider, setProvider ] = useState<IProvider | null>(null);
 
+  const pregenerateWallet = async () => {
+
+    const uuid = uuidv4();
+    setUuid(uuid);
+
+    const lookupURL = `https://lookup.web3auth.io/lookup?verifier=untitled-game-01-discord-verifier&verifierId=${uuid}&web3AuthNetwork=sapphire_devnet&clientId=${process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID}&`;
+
+    const res = await fetch(lookupURL);
+    const data = await res.json();
+
+    console.log('data:', data);
+
+    if (data.success) {
+      setAddress(data.data.evmAddress);
+    }
+
+  }
 
   const signInWithDiscord = async () => {
-    window.location.href = `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Flogin%2Fdiscord&scope=identify+email`;
+    if (!uuid) { await pregenerateWallet(); }
+    window.location.href = `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Flogin%2Fdiscord&scope=identify+email&state=${uuid}`;
   }
 
   const login = async (token: string) => {
@@ -49,17 +70,25 @@ export default function Web3AuthProvider({ children }: any) {
     localStorage.setItem('token', token);
     localStorage.setItem('payload', JSON.stringify(payload));
 
-    const web3authProvider = await web3auth.connect({
-      verifier: 'untitled-game-01-discord-verifier',
-      verifierId: (payload as any).sub,
-      idToken: token,
-    });
+    try {
+      const web3authProvider = await web3auth.connect({
+        verifier: 'untitled-game-01-discord-verifier',
+        verifierId: (payload as any).sub,
+        idToken: token,
+      });
 
-    if (web3authProvider) {
-      setProvider(web3authProvider);
-      setLoggedIn(true);
-      getUserInfo();
+      console.log('web3authProvider:', web3authProvider);
+
+      if (web3authProvider) {
+        setProvider(web3authProvider);
+        setLoggedIn(true);
+        getUserInfo();
+      }
+    } catch (error) {
+      console.log('error in login');
+      console.error(error);
     }
+
   }
 
 
@@ -98,7 +127,6 @@ export default function Web3AuthProvider({ children }: any) {
   
     const token = params.get('token');
 
-    
     if (token) {
       login(token);
     }
